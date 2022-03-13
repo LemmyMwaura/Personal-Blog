b2739ab263ebf050d4d0e08ce8d8893959703d78
Lemmy Mwaura <lemminton@gmail.com>
2022-03-13 20:42:08 +0300

    initial files

---
+++ config.py
@@ -0,0 +1,4 @@
+# class Config():
+#     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+#     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@localhost/pitchesapp'
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
