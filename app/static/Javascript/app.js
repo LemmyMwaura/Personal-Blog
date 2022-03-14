f6c10f3e80664db54d1fefe26f95c9f1056117f8
Lemmy Mwaura <lemminton@gmail.com>
2022-03-14 19:38:29 +0300

    Project refactor

--- app/static/Javascript/app.js
+++ app/static/Javascript/app.js
@@ -41,7 +41,7 @@ function like(postId) {
   const likesCount = document.getElementById(`likes-count-${postId}`)
   const likesButton = document.getElementById(`like-button-${postId}`)
 
-  fetch(`/posts/like-post/${postId}`, {method: "POST"})
+  fetch(`/blogs/like-post/${postId}`, {method: "POST"})
     .then((res) => res.json())
     .then((data) => {
       console.log(data)
